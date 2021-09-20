0a5007c4d247607a87f03cdadd2d36ee403c7c88
breezy <apelibrian@gmail.com>
2021-09-21 02:03:33 +0300

    updated functioning canvas

--- components/test.js
+++ components/test.js
@@ -1,16 +1,22 @@
 import React, { useState, useRef, useEffect } from 'react';
 
-
 const Test = () => {
     const vplayer = useRef(null)
     const canvasRef = useRef(null);
+    console.log('height', vplayer.height);
     useEffect(() => {
+        /**
+     * 1. Select video and canvas
+     * 2. Get canvas context
+     */
         const canvas = document.querySelector('canvas');
         console.log('canvas', canvas);
+
         const video = document.querySelector('video');
         console.log('video', video);
+
         const ctx = canvas.getContext('2d');
-        console.log('ctx', ctx);
+        console.log('ctx', ctx)
 
         /**
     * Set canvas dimensions
@@ -20,23 +26,22 @@ const Test = () => {
             canvas.width = video.videoWidth;
         });
 
-
         /**
-         * 1. Start drawing on play
-         * 2. Redraw
-         */
+     * 1. Start drawing on play
+     * 2. Redraw
+     */
         const frameRate = 24;
-
         video.addEventListener('play', () => {
             const drawImage = () => {
                 ctx.drawImage(video, 0, 0);
-                if (!video.paused) {
-                    setTimeout(drawImage, 1000 / frameRate);
-                }
+                if(!video.paused){
+                    setTimeout(drawImage, 1000/ frameRate);
+                } 
+                
             }
-            setTimeout(drawImage, 1000 / frameRate);
+            setTimeout(drawImage, 1000/frameRate);
+            console.log('ctx', ctx);
         })
-
 
     }, []);
 
@@ -44,7 +49,7 @@ const Test = () => {
     return (
         <div>
 
-            <video src="https://www.html5rocks.com/tutorials/video/basics/Chrome_ImF.webm" autoPlay muted loop controls></video>
+            <video id='video' src='https://res.cloudinary.com/dogjmmett/video/upload/v1632176814/cisne_1_wtkyzm.mp4' autoPlay muted loop controls></video>
             <canvas></canvas>
         </div>
     )
